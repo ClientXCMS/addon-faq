a908a09c3461bf222f12745a39ee0310075e335b
alexwrite <a.eteocle@cerbonix.net>
2026-02-15 00:06:17 +0100

    fix(routes): use /faqs as canonical URL, redirect /faq to /faqs

--- addons/faq/routes/web.php
+++ addons/faq/routes/web.php
@@ -2,9 +2,9 @@
 
 use App\Addons\Faq\Controllers\Client\FaqController;
 
-Route::redirect('faqs', 'faq', 301);
+Route::redirect('faq', 'faqs', 301);
 
-Route::prefix('faq')->name('client.faq.')->group(function () {
+Route::prefix('faqs')->name('client.faq.')->group(function () {
     Route::get('/', [FaqController::class, 'index'])->name('index');
     Route::get('/group/{group:slug}', [FaqController::class, 'group'])->name('group');
     Route::get('/product/{product}', [FaqController::class, 'product'])->name('product');
